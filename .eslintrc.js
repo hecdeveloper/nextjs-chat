385a907adc969b0ff414ac73f4a70717e7795dc4
Hector Ibarra <hector@oneseventech.com>
2022-07-17 14:27:22 -0400

    configs for accesibility

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
-    
+    root: true,
+    env: {
+        browser: true,
+        amd: true,
+        node: true,
+        es6: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:prettier/recommended'
+    ]
 }
